197ef7f6134e2ac799a90d3dcc030f7858149b90
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-12 21:38:52 -0300

    Update exercicio7.pas
    Resolução exercício para calcular área do retângulo

--- exercicios/exercicio7.pas
+++ exercicios/exercicio7.pas
@@ -1,11 +1,13 @@
-{
-  Objetivo do programa: Calcule a área de um retângulo
-					    Area = comprimento * altura
-  Data da criacao: 2025-08-28
-  Criado por: @programacaomentoria
-}  
+//Calcule a área de um retângulo. Area = comprimento * altura
 program exercicio7;
-
-begin:
-
+var
+    altura, comprimento, area: integer;
+begin
+    writeln ('Para saber a area do retangulo, informe os valores solicitado: ');
+    writeln ('Altura:');
+    readln (altura);
+    writeln ('Comprimento:');
+    readln (comprimento);
+    area := comprimento * altura;
+    writeln ('A area do retângulo é ',area);
 end.
