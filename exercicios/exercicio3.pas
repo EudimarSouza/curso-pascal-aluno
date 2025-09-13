3c60a831cb14667a44c019e704a13556ffe74591
Eudimar Souza <90109854+EudimarSouza@users.noreply.github.com>
2025-09-12 21:07:27 -0300

    Update exercicio3.pas
    Exercício de media do aluno

--- exercicios/exercicio3.pas
+++ exercicios/exercicio3.pas
@@ -1,10 +1,22 @@
-{
-  Objetivo do programa: Perguntar o nome de um aluno, pergunte as 4 notas e calcule a média aritmética, exibindo no final.
-  Data da criacao: 2025-08-28
-  Criado por: @programacaomentoria
-}  
+// Perguntar o nome de um aluno, pergunte as 4 notas e calcule a média aritmética(média simples), exibindo no final.
 program exercicio3;
-
-begin:
-
+var
+    nome: string;
+    nota1, nota2, nota3, nota4, soma: integer;
+    media: real;
+begin
+    writeln ('Qual o nome do aluno? ');
+    readln (nome);
+    writeln ('Digite as notas do aluno: ');
+    writeln ('Primeira Nota: ');
+    readln (nota1);
+    writeln ('Segunda Nota: ');
+    readln (nota2);
+    writeln ('Terceira Nota: ');
+    readln (nota3);
+    writeln ('Quarta Nota: ');
+    readln (nota4);
+    soma := nota1 + nota2 + nota3 + nota4;
+    media := soma/4;
+    writeln ('O aluno ',nome , 'tem a media de ', media:0:2);
 end.
